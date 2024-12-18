70f381cb5e80fc119f8314dd1d9127827bd33b19
Francesco Copelli <francescocopelli28@gmail.com>
2024-12-18 01:01:38 +0100

    Update testCredenziali.js
    Fetch config parameter from config

--- testCredenziali.js
+++ testCredenziali.js
@@ -1,6 +1,11 @@
-fetch('http://localhost:3000/api/test-credentials')
+fetch('/config').then(response => response.json()).then(config => {
+
+
+fetch(`http://${config.SERVER_HOST}:${config.SERVER_PORT}/api/test-credentials`)
 .then(response => response.json())
 .then(data => {
     console.log('Credenziali utenti:', data);
 })
 .catch(error => console.error('Errore:', error));
+
+});
