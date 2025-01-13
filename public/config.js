3b5a06eeb746f7fec1d83adedf555cdc0aaa8bb5
AndrewChop <amarantoandreagmail.com>
2025-01-13 17:32:14 +0100

    Update config front-end

--- public/config.js
+++ public/config.js
@@ -2,9 +2,3 @@ window.config = {
     serverUrl: 'https://swen-esn-management-web-platform.onrender.com',
     webSocketUrl: 'wss://swen-esn-management-web-platform.onrender.com',
 };
-
-// URL per le chiamate HTTP/REST
-window.config.serverUrl = `http://${window.config.serverHost}:${window.config.serverPort}`;
-
-// URL per la connessione WebSocket
-window.config.webSocketUrl = `ws://${window.config.serverHost}:${window.config.serverPort}`;
